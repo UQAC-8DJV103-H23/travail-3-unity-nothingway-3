63af9534f94fd10da1e101a5bb3e1f79203c3833
Lea1Bouchard <lbouchar14@etu.uqac.ca>
2023-03-16 20:19:30 -0400

    Feat : added proceduraly generated forest, add new asset (low poly water)

--- Assets/Game/Scripts/forestGenerator.cs
+++ Assets/Game/Scripts/forestGenerator.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class forestGenerator : MonoBehaviour
 {
+    [SerializeField] Rect spawnRect;
+    [SerializeField] GameObject[] assetToInstanciate;
+    [SerializeField] int nbAssetToSpawn;
+    [SerializeField] GameObject parent;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GenerateForest();
     }
 
     // Update is called once per frame
-    void Update()
+    private void GenerateForest()
     {
-        
+        Vector3 position;
+        for (int i = 0; i < nbAssetToSpawn; i++)
+        {
+            int indexToInstantiate = Random.Range(0, assetToInstanciate.Length);
+
+            position.x = ProceduralToolkit.RandomE.PointInRect(spawnRect).x;
+            position.z = ProceduralToolkit.RandomE.PointInRect(spawnRect).y;
+            position.y = GetGroundPosition(new Vector3(position.x, 100f, position.z));
+
+            Instantiate(assetToInstanciate[indexToInstantiate], position, assetToInstanciate[indexToInstantiate].transform.rotation, parent.transform);
+        }
+    }
+
+
+    private float GetGroundPosition(Vector3 position)
+    {
+        RaycastHit hit;
+        bool raycastHit = Physics.Raycast(position, Vector3.down, out hit);
+        if (raycastHit)
+        {
+            return hit.point.y;
+        }
+        return -1f;
     }
 }
