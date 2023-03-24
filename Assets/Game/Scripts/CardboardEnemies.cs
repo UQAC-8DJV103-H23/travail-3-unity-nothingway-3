053ca38789e4c57f2d5d66067a9c79e80b261281
Falerhon <alex-lex360@hotmail.com>
2023-03-23 22:01:00 -0400

    Feat : Added walls and scarecrows to the fortresses and added script so the wall gets destroyed

--- Assets/Game/Scripts/CardboardEnemies.cs
+++ Assets/Game/Scripts/CardboardEnemies.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class CardboardEnemies : MonoBehaviour
 {
     public QuestManager questManager;
+    public Wall wall;
+
     private void OnTriggerEnter(Collider other)
     {
 
         if(other.CompareTag("Projectile"))
         {
             questManager.Slay("Scarecrow");
+
+            wall.ReduceScarecrowsToKill();
 
             Destroy(gameObject);
 
